b70b06f26a6de72f5ba229dc983c745dc8e2e06b
Meseker <meseker@ubuntu.ubuntu-domain>
2012-07-05 14:07:13 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vituosite::Application.config.secret_token = 'ec8b6e6a05812174d254771e2e6f94cf9de687eff7e865c6aa1470588d5f697815d4bde784d305fbf6c1ba3a80342662bcc61a77a47263eeb1534e155667b426'
